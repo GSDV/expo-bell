b3d0143f5a4e3ad3e0db89c7ddaf21896d0dfb83
GSDV <sdvgab@gmail.com>
2025-08-14 09:34:58 -0400

    Storing token retrieval

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -2,7 +2,19 @@ import * as SecureStore from 'expo-secure-store';
 
 
 
-const EXPO_PUSH_NOTIFICATION_TOKEN_KEY = 'expo-bell/expo-push-notification-key';
+const LAST_TOKEN_RETRIEVAL_TIMESTAMP_KEY = 'expo-bell/last-token-retrieval-key';
+const EXPO_PUSH_NOTIFICATION_TOKEN_KEY = 'expo-bell/expo-push-notification-token-key';
+
+
+
+export const getTokenRetrievalTimestamp = async () => {
+    const pushToken = await SecureStore.getItemAsync(LAST_TOKEN_RETRIEVAL_TIMESTAMP_KEY);
+    return pushToken;
+};
+
+export const storeTokenRetrievalTimestamp = async (val: string) => {
+    await SecureStore.setItemAsync(LAST_TOKEN_RETRIEVAL_TIMESTAMP_KEY, val);
+};
 
 
 
